40ca370919492b97e758b596f8566f5fffa2aa96
Pieter Provoost <pieterprovoost@gmail.com>
2023-09-07 12:26:34 +0200

    validate json

--- validation/scripts/validate.py
+++ validation/scripts/validate.py
@@ -1,21 +1,45 @@
 import os
 import rdflib
 from pyshacl import validate
+import json
+
 
 shape_graph = rdflib.Graph()
 shape_graph.parse("validation/shacl/dataset-shape.ttl", format="turtle")
 
-files = [os.path.join("datasets", f) for f in os.listdir("datasets") if os.path.isfile(os.path.join("datasets", f))]
 
-for f in files:
-    print(f)
+def validate_json(jsonld: str):
+    try:
+        json.loads(jsonld)
+    except ValueError as err:
+        return False, None
+    return True, None
+
+
+def validate_rdf(jsonld: str):
     dataset_graph = rdflib.Graph()
-    dataset_graph.parse(f, format="json-ld")
-
+    dataset_graph.parse(data=jsonld, format="json-ld")
     conforms, results_graph, results_text = validate(dataset_graph, shacl_graph=shape_graph, inference="rdfs")
 
     if conforms:
-        print("Validation successful")
+        return True, None
     else:
-        print("Validation failed")
-        print(results_text)
+        return False, results_text
+
+
+dataset_files = [os.path.join("datasets", f) for f in os.listdir("datasets") if os.path.isfile(os.path.join("datasets", f))]
+
+for dataset_filename in dataset_files:
+
+    print(dataset_filename)
+
+    dataset_file = open(dataset_filename, "r")
+    jsonld = dataset_file.read()
+    dataset_file.close()
+
+    json_ok, json_results = validate_json(jsonld)
+    if json_ok:
+        rdf_ok, rdf_results = validate_rdf(jsonld)
+        print(rdf_results)
+
+    print(json_ok, rdf_ok)
