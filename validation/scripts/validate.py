ae18d0952b092268929d7a80a618f49ac64e350c
Pieter Provoost <pieterprovoost@gmail.com>
2023-09-08 16:21:47 +0200

    added templating

--- validation/scripts/validate.py
+++ validation/scripts/validate.py
@@ -2,6 +2,7 @@ import os
 import rdflib
 from pyshacl import validate
 import json
+from jinja2 import Environment, FileSystemLoader
 
 
 shape_graph = rdflib.Graph()
@@ -24,32 +25,42 @@ def validate_rdf(jsonld: str):
     if conforms:
         return True, None
     else:
-        return False, results_text
+        return False, results_graph
 
 
 dataset_files = [os.path.join("datasets", f) for f in os.listdir("datasets") if os.path.isfile(os.path.join("datasets", f))]
 
-output = ""
+context = {"datasets": []}
+environment = Environment(loader=FileSystemLoader("docs/_includes/"))
+template = environment.get_template("datasets_template.html")
 
 for dataset_filename in dataset_files:
-
-    output = output + f"## [{dataset_filename}](https://github.com/marco-bolo/dataset-catalogue/blob/main/{dataset_filename})\n\n"
 
     dataset_file = open(dataset_filename, "r")
     jsonld = dataset_file.read()
     dataset_file.close()
 
     json_ok, json_results = validate_json(jsonld)
-    output = output + f"- JSON valid: {json_ok}\n\n"
-
     if json_ok:
         rdf_ok, rdf_results = validate_rdf(jsonld)
-        output = output + f"- RDF valid: {rdf_ok}\n\n"
+
+    dataset = {
+        "filename": dataset_filename,
+        "document_url": f"https://github.com/marco-bolo/dataset-catalogue/blob/main/{dataset_filename}",
+        "json_valid": json_ok,
+        "rdf_valid": rdf_ok,
+        "rdf_results": {}
+    }
 
     if not rdf_ok:
+        for subject, predicate, obj in rdf_results:
+            if subject.toPython() not in dataset["rdf_results"]:
+                dataset["rdf_results"][subject.toPython()] = {}
+            if predicate.toPython() == "http://www.w3.org/ns/shacl#resultMessage" or predicate.toPython() == "http://www.w3.org/ns/shacl#resultPath" or predicate.toPython() == "http://www.w3.org/ns/shacl#resultSeverity":
+                dataset["rdf_results"][subject.toPython()][predicate.toPython().replace("http://www.w3.org/ns/shacl#", "")] = obj.toPython()    
+        dataset["rdf_results"] = [dataset["rdf_results"][key] for key in dataset["rdf_results"] if len(dataset["rdf_results"][key]) > 0]
 
-        output = output + f"```\n{rdf_results}\n```"
+    context["datasets"].append(dataset)
 
-    output_file = open("docs/_includes/datasets.html", "w")
-    output_file.writelines(output)
-    output_file.close()
+with open("docs/_includes/datasets.html", mode="w", encoding="utf-8") as html_file:
+    html_file.write(template.render(context))
